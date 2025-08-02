6d93f50b90faf52c3c8649cf39093de40f265a76
Max <halimovmahmadali2005@gmail.com>
2025-08-02 13:31:32 +0500

    🔧 Fix GitHub Pages: правильные пути к ресурсам с /about-me/ префиксом

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -9,7 +9,7 @@ module.exports = {
         hashFunction: 'xxhash64',
         filename: 'bundle.[contenthash].js',
         path: path.resolve(__dirname, '../public'),
-        publicPath: process.env.NODE_ENV === 'production' ? '/about-me/' : '/',
+        publicPath: process.env.NODE_ENV === 'production' || process.env.CI ? '/about-me/' : '/',
     },
     devtool: 'source-map',
     plugins: [
